0df59b095d89fb15fe8950450276dbe3301ae89e
Alex Carreira <aycarrei@gmail.com>
2018-05-18 22:03:11 -0600

    Add getRawFile--the original getFile functionality from blockstack. Add getPublicKeyFromPrivate.

---
+++ Blockstack/Classes/BlockstackInstsance+Stealthy.swift
@@ -0,0 +1,38 @@
+//
+//  BlockstackInstance+Swift.swift
+//  Blockstack
+//
+//  Created by AC sometime in May 2018.
+//
+
+import Foundation
+
+public extension BlockstackInstance {
+
+    // This method conforms to the original Blockstack Javascript API behavior, returning a
+    // string or buffer and not trying to convert it to JSON.
+    //
+    @objc public func getFileRaw(path: String, completion: @escaping (Any?, Error?) -> Void) {
+        let translatedCompletion: (Any?, GaiaError?) -> Void = { value, error in
+            completion(value, error)
+        }
+
+        Gaia.sharedSession().getOrSetLocalHubConnection { error in
+            guard error == nil else {
+                print("gaia connection error")
+                completion(nil, error)
+                return
+            }
+
+            Gaia.sharedSession().getFileRaw(path: path, completion: translatedCompletion)
+        }
+    }
+
+    // Stealthy uses this to get an app public key from the app private key. (This appears to
+    // be different than the public key returned in the user profile.
+    //
+    @objc public func getPublicKeyFromPrivate(aPrivateKey: String) -> String? {
+        let publicKey = Keys.getPublicKeyFromPrivate(aPrivateKey);
+        return publicKey;
+    }
+}
---
+++ Blockstack/Classes/GaiaSession+Stealthy.swift
@@ -0,0 +1,30 @@
+//
+//  GaiaSession+Swift.swift
+//  Blockstack
+//
+//  Created by AC sometime in May 2018.
+//
+
+import Foundation
+
+public extension GaiaSession {
+    func getFileRaw(path: String, completion: @escaping (Any?, GaiaError?) -> Void) {
+        guard let config = self.config else {
+            return
+        }
+        
+        let fullReadURLString = "\(config.URLPrefix!)\(config.address!)/\(path)"
+        let fullReadURL = URL(string: fullReadURLString)
+        
+        let task = URLSession.shared.dataTask(with: fullReadURL!) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Gaia hub store request error")
+                completion(nil, GaiaError.requestError)
+                return
+            }
+            
+            completion(data, nil)
+        }
+        task.resume()
+    }
+}
